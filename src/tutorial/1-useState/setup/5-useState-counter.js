96283a8ae1ea8794cf9315eb27cc5bb2a762e380
Shreyas Hegde <75136935+Shreyas-hacker@users.noreply.github.com>
2022-07-01 21:32:29 +0800

    useState done

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,40 @@
 import React, { useState } from 'react';
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  const [number,setNumber] = useState(0);
+
+  const addNumber = ()=>{
+    setNumber(number+1)
+  }
+  const subNumber = ()=>{
+    if(number>0){
+      setNumber(number-1)
+    }
+  }
+  const complexIncrease = ()=>{
+    setTimeout(()=>{
+      // setNumber(number+1)
+      setNumber((prevState)=>{
+        return prevState+1;
+      })
+    },2000)
+  }
+  return (
+    <>
+      <section style={{margin:'4rem 0'}}>
+        <h2>regular counter</h2>
+        <h1>{number}</h1>
+        <button className='btn' onClick={addNumber}>Increase</button>
+        <button className='btn' onClick={()=>setNumber(0)}>Reset</button>
+        <button className='btn' onClick={subNumber}>Decrease</button>
+      </section>
+      <section style={{margin:'4rem 0'}}>
+        <h2>More complex counter</h2>
+        <h1>{number}</h1>
+        <button className='btn' onClick={complexIncrease}>Increase later</button>
+      </section>
+    </>
+  );
 };
 
 export default UseStateCounter;
